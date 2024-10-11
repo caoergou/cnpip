c1dabf9cd9a7b7abb854b5c069911ed9a1ce3bef
Eric Cao <caofuguo@foda.ai>
2024-10-11 19:33:16 +0800

    style: add pyproject.toml.

--- setup.py
+++ setup.py
@@ -6,12 +6,26 @@ setup(
     description="帮助中国用户快速切换 pip 镜像源，提升下载速度的命令行工具/A tool that helps Chinese users quickly switch pip mirrors to improve download speeds.",
     author="caoergou",
     author_email="itsericsmail@gmail.com",
+    url="https://github.com/caoergou/cnpip",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'cnpip=cnpip.cnpip:main',  # 指定命令行工具的入口
+            'cnpip=cnpip.cnpip:main',
         ],
     },
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: Utilities",
+    ],
     install_requires=[
         'requests'
     ],
