96cc9ffdef6f58d6784ed0dd8e32e58a06343b74
Eric Cao <caofuguo@foda.ai>
2024-10-12 12:40:03 +0800

    upgrade: upgrade version to 1.0.5.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cnpip",
-    version="1.0.4",
+    version="1.0.5",
     description="帮助中国用户快速切换 pip 镜像源，提升下载速度的命令行工具/A tool that helps Chinese users quickly switch pip mirrors to improve download speeds.",
     author="caoergou",
     author_email="itsericsmail@gmail.com",
